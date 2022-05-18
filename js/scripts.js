c6ff105dee7b654dcc28ad0964199c04eb233bd1
John Bataller <johnanthonybataller.ce@gmail.com>
2022-05-18 18:44:00 +0800

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,100 @@
+let subscribeBtn = document.querySelector("#subscribeBtn")
+
+if (subscribeBtn) {
+  subscribeBtn.addEventListener("click", function(e){
+    e.preventDefault()
+  
+    Swal.fire({
+      title: 'Success!',
+      text: 'Thank you for subscribing to i-Lab Result .',
+      icon: 'success',
+      confirmButtonText: 'Close'
+    })
+  })
+}
+
+let sendBtn = document.querySelector("#sendBtn")
+
+if(sendBtn) {
+  sendBtn.addEventListener("click", function(e){
+    e.preventDefault()
+  
+    Swal.fire({
+      title: 'Success!',
+      text: 'Your message has been sent successfully.',
+      icon: 'success',
+      confirmButtonText: 'Close'
+    })
+  })
+}
+
+let checkoutBtn = document.querySelector("#checkoutBtn")
+
+if(checkoutBtn) {
+  checkoutBtn.addEventListener("click", function(e){
+    e.preventDefault()
+  
+    Swal.fire({
+      title: 'Success!',
+      text: 'Thank you! Your Appointment has been set. ',
+      icon: 'success',
+      confirmButtonText: 'Close'
+    })
+
+    setTimeout(function(){ window.location.href='home.html' }, 2000);
+  })
+
+  
+}
+
+
+
+let addToCartButton = document.querySelectorAll(".add-to-cart-button");
+
+document
+  .querySelectorAll(".add-to-cart-button")
+  .forEach(function (addToCartButton) {
+    addToCartButton.addEventListener("click", function () {
+      
+      // INCREMENT CART VALUE
+      var value = parseInt(document.getElementById('cartCount').value, 10);
+      value = isNaN(value) ? 0 : value;
+      value++;
+      document.getElementById('cartCount').value = value;
+      document.getElementById('cartCount').innerHTML = "";
+      document.getElementById('cartCount').innerHTML = ` <i class="fas fa-stethoscope"></i> ${value}`;
+
+      addToCartButton.classList.add("added");
+    });
+  });
+
+  $(document).ready(function () {
+    $(".customer-logos").slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      arrows: false,
+      dots: false,
+      pauseOnHover: false,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 4
+          }
+        },
+        {
+          breakpoint: 520,
+          settings: {
+            slidesToShow: 3
+          }
+        }
+      ]
+    });
+  });
+
+
+
+
+
